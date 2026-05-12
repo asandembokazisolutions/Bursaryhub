c77949343ab1ec2daaac5a17d581eecec95e723a
Asande Mbokazi <asandeseaphesihle@gmail.com>
2026-05-12 04:30:32 +0200

    Set IsEmailVerified to true for admin user

--- Bursaryhub/Data/DbSeeder.cs
+++ Bursaryhub/Data/DbSeeder.cs
@@ -27,7 +27,7 @@ public static class DbSeeder
                 PhoneNumber     = "0612345678",
                 RoleId          = 1,
                 IsActive        = true,
-                IsEmailVerified = false,
+                IsEmailVerified = true,
                 CreatedDate     = DateTime.UtcNow,
             };
             db.Users.Add(admin);
